fffda8748e39bb66bc6f8dcfb74d8e67788543ad
Keshav <keshavtarafdar@gmail.com>
2025-03-28 21:50:01 -0400

    Start creating components, starting with a puzzle Cell

---
+++ sudoku-app/src/components/Cell.jsx
@@ -0,0 +1,92 @@
+/**
+ * A single "cell" in the Sudoku grid.
+ * 
+ * - either shows a number (1-9) or an empty cell
+ * - allows user to input a number
+ * - highlights the cell when selected
+ * - support notes/pencil marks
+ */
+
+import React, { useState } from 'react';
+
+/**
+ * Components are functions that return JSX - a JSX element is a combination of JS
+ * and HTML tags. This is a functional component.
+ * 
+ * value - current displayed value
+ * isEditable & isNoteMode - booleans
+ * onChange - callback function for when value changes
+ * onSelect - callback function for when cell is selected
+ * notes - array of notes
+*/ 
+const Cell = ({value, isEditable, isNotesMode, onChange, isSelected, onSelect, notes}) => {
+
+    /**
+     * - creates an inputValue variable WITH STATE, setting it to the value parameter 
+     * if it was passed in, else just '' (empty square)
+     * - creates a setInputValue function, which when called will only take one
+     * parameter - the new value of inputValue, and SAVE STATE
+     */
+    const [inputValue, setInputValue] = useState(value || '');
+    const [cellNotes, setCellNotes] = useState(notes || []);
+
+    const handleInputChange = (e) => {
+        /**
+         * e, the event object, stores user input here.
+         * 
+         * target is the DOM element that triggered the event (ex. an input)
+         * value is the current content of the input field
+         * 
+        */
+        const input = e.target.value;
+
+        if(isNotesMode) {
+            if(input >= '1' && input <= '9' && !cellNotes.includes(input)){
+                // no append function - must use spread operator
+                setCellNotes([...cellNotes, input].sort());
+            }
+        } else {
+            if(input == '' || input >= '1' && input <= '9') {
+                setInputValue(input);
+                onChange(input);
+            }
+        }
+    };
+
+    const handleFocus = () => { onSelect(); };
+
+    return (
+        <div
+            /**
+             * Main container for the cell - sets class name based on if
+             * the Cell is editable and/or selectable to allow easy styling
+             * in a CSS file!
+             */
+            className={`cell ${isEditable ? 'editable' : ''} ${isSelected ? 'selected' : ''}`}
+            onClick={handleFocus}
+        >
+            {isNotesMode && isEditable ? ( // ternary operator!
+                <div className='notes'>
+                    {cellNotes.map((note) => (
+                        /**
+                         *  <span>s are inline elements which can be styled uing
+                         * CSS to appear smaller and in a grid! :)
+                        */
+                        <span key={note} className='note'>{note}</span>
+                    ))}
+                </div>
+            ) : ( // render for direct entry
+                <input
+                    type='text'
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    disabled={!isEditable}
+                    maxLength='1'
+                    className='cell-input'
+                />
+            )}
+        </div>
+    );
+};
+
+export default Cell;
